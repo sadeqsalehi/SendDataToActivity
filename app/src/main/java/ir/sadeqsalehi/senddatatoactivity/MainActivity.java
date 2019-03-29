b268c78a60d632f38e3ece5f3aa45a86ebb40c0e
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-29 16:00:36 +0430

    setResult , Confirm Data

--- app/src/main/java/ir/sadeqsalehi/senddatatoactivity/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/senddatatoactivity/MainActivity.java
@@ -1,14 +1,17 @@
 package ir.sadeqsalehi.senddatatoactivity;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtUserName;
+    public static final int REQUEST_CODE=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
     public void sendDataOnClick(View view) {
         Intent intent= new Intent(getApplicationContext(),SecondActivity.class);
         intent.putExtra("username",txtUserName.getText().toString());
-        startActivity(intent);
+        //startActivity(intent);
+        startActivityForResult(intent,REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode==RESULT_OK){
+            Toast.makeText(this, "Confirmed", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            Toast.makeText(this, "Not Confirmed!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/ir/sadeqsalehi/senddatatoactivity/SecondActivity.java
+++ app/src/main/java/ir/sadeqsalehi/senddatatoactivity/SecondActivity.java
@@ -1,25 +1,46 @@
 package ir.sadeqsalehi.senddatatoactivity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView txtUserName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        txtUserName=findViewById(R.id.tvUserName);
+        txtUserName = findViewById(R.id.tvUserName);
         Bundle extras = getIntent().getExtras();
-        if (extras!=null){
+        if (extras != null) {
             String userName;
-            if (extras.containsKey("username")){
-                userName= String.format("Welcome : %s", getIntent().getExtras().get("username"));
+            if (extras.containsKey("username")) {
+                userName = String.format("Welcome : %s", getIntent().getExtras().get("username"));
                 txtUserName.setText(userName);
             }
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem confirm = menu.add("Confirm");
+        confirm.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        confirm.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
