72453852d8cfff4bbd22b123b7d30b89b50392e2
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-29 15:10:12 +0430

    putExtras , Bundle ,getExtras

--- app/src/main/java/ir/sadeqsalehi/senddatatoactivity/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/senddatatoactivity/MainActivity.java
@@ -1,13 +1,24 @@
 package ir.sadeqsalehi.senddatatoactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUserName=findViewById(R.id.edUserName);
+    }
+
+    public void sendDataOnClick(View view) {
+        Intent intent= new Intent(getApplicationContext(),SecondActivity.class);
+        intent.putExtra("username",txtUserName.getText().toString());
+        startActivity(intent);
     }
 }
--- app/src/main/java/ir/sadeqsalehi/senddatatoactivity/SecondActivity.java
+++ app/src/main/java/ir/sadeqsalehi/senddatatoactivity/SecondActivity.java
@@ -1,13 +1,25 @@
 package ir.sadeqsalehi.senddatatoactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    TextView txtUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        txtUserName=findViewById(R.id.tvUserName);
+        Bundle extras = getIntent().getExtras();
+        if (extras!=null){
+            String userName;
+            if (extras.containsKey("username")){
+                userName= String.format("Welcome : %s", getIntent().getExtras().get("username"));
+                txtUserName.setText(userName);
+            }
+
+        }
     }
 }
